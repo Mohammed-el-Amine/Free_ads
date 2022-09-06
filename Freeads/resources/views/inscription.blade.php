d83962c524caeb38759661c4c81f488448961065
amine <mohammed-el-amine.djellal@epitech.eu>
2022-09-06 11:22:41 +0200

    [ADD] file laravel project

---
+++ Freeads/resources/views/inscription.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+
+<body>
+    <form method="get">
+        <input type="text" name="firstname" placeholder="Prénom">
+        <!-- <input type="text" name="lastname" placeholder="Nom"><br> -->
+        <input type="email" name="email" placeholder="E-mail">
+        <input type="password" name="password" placeholder="Mot de passe">
+        <input type="password" name="password_confirmation" placeholder="Mot de passe (confirmation)">
+        <input type="submit" value="S'inscrire">
+    </form>
+</body>
+
+</html>
